789047d5aef9111e37850145ce67b765e76d573a
Veyga <marius.memmel@gmx.net>
2018-06-28 11:59:45 +0200

    New Project, old project loading whole dataset - ODataQuery working

---
+++ ProjectOneNoEF/Demo/Controllers/PeopleController.cs
@@ -0,0 +1,21 @@
+﻿using Demo.DataSource;
+using MongoDB.Bson;
+using MongoDB.Bson.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.OData;
+
+namespace Demo.Controllers
+{
+    [EnableQuery]
+    public class PeopleController : ODataController
+    {
+        public IHttpActionResult Get()
+        {
+            return Ok(DemoDataSources.Instance.People.AsQueryable());
+        }
+    }
+}
